498b3fc5e9af54ce5c28b50fca01097d0c5259ae
cenxui <b97201017@ntu.edu.tw>
2018-05-30 14:29:24 +0800

    rename to sam

--- vc/test/camera/api_test.go
+++ vc/test/camera/api_test.go
@@ -12,7 +12,7 @@ import (
 
 
 func TestGet(t *testing.T) {
-	resp, err := http.Get("https://nvbgipgj47.execute-api.ap-northeast-1.amazonaws.com/dev/camera/123456789")
+	resp, err := http.Get("https://nvbgipgj47.execute-sam.ap-northeast-1.amazonaws.com/dev/camera/123456789")
 	if err != nil {
 	// handle error
 	}
@@ -25,7 +25,7 @@ func TestPost(t *testing.T) {
 	camera := repository.Camera{MAC:"gggg", Camera:"0524"}
 	c, _ := json.Marshal(camera);
 
-	resp, _ := http.Post("https://nvbgipgj47.execute-api.ap-northeast-1.amazonaws.com/dev/camera",
+	resp, _ := http.Post("https://nvbgipgj47.execute-sam.ap-northeast-1.amazonaws.com/dev/camera",
 		"application/x-www-form-urlencoded", strings.NewReader(string(c)))
 
 	defer resp.Body.Close()
